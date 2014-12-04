fc68fb49dfac665d536d2a58ff14c262ca8f0292
Julia <julia.zuara.jimenez@gmail.com>
2014-12-04 18:11:38 +0100

    Añadidos metodos de bebedor

--- Bebedor.java
+++ Bebedor.java
@@ -7,27 +7,84 @@
  */
 public class Bebedor
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Nombre del bebedor
+    private String nombre;
+    // Cantidad de alcohol en sangre
+    private int alcoholEnSangre;
+    // Limite de alcohol que puede tener en sangre
+    private int limiteAlcohol;
 
     /**
-     * Constructor for objects of class Bebedor
+     * Constructor para los bebedores. Introduce el nombre y cuanto alcohol aguanta.
      */
-    public Bebedor()
+    public Bebedor(String nombre, int limiteAlcohol)
     {
-        // initialise instance variables
-        x = 0;
+        // Inicializa las variables, el alcohol en sangre siempre inicia en 0
+        this.nombre = nombre;
+        this.limiteAlcohol = limiteAlcohol;
+        alcoholEnSangre = 0;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Da una copa al bebedor, aumenta su alcohol en sangre
      */
-    public int sampleMethod(int y)
+    public void darCopa(Cubata nombreCopa)
     {
-        // put your code here
-        return x + y;
+        // Comprobamos si puede aguantar la copa que le damos, si es asi la bebe, sino la rechaza
+        if (haLlegadoAlLimite())
+        {
+            System.out.println("No quiero más copas");
+        }
+        else
+        {
+            // Al beber la copa, el nivel de alcohol en sangre se incrementa en la 
+            //cuantia de la cantidad de alcohol de la bebida
+            alcoholEnSangre = alcoholEnSangre + nombreCopa.getCantidadAlcohol();
+        }
+    }
+
+    /**
+     * Hace una pregunta al bebedor
+     */
+    public void hacerPreguntas(String pregunta)
+    {
+        // Comprobamos primero si se ha pasado del limite de alcohol en sangre
+        if ((haLlegadoAlLimite()) || (pregunta.contains(nombre)))
+        {
+            // Si se ha pasado del limite o contiene su nombre, responde con la pregunta gritando
+            pregunta = pregunta.toUpperCase();
+            System.out.println(pregunta);
+        }
+        else
+        {
+            // Si no se ha pasado del limite, responde con si si el numero de caracteres
+            // de la pregunta es par y no si es impar
+            if (((pregunta.length())%2) == 0)
+            {
+                System.out.println("Si");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+    }
+
+    /**
+     * Comprueba si el bebedor se ha pasado del limite de alcohol en sangre que
+     * tenga, devuelve un boolean true si se ha pasado, false si no.
+     */
+    private boolean haLlegadoAlLimite()
+    {
+        boolean bebidoDemasiado = false;
+        if (limiteAlcohol < alcoholEnSangre)
+        {
+            bebidoDemasiado = true;
+        }
+        else
+        {
+            bebidoDemasiado = false;
+        }
+        return bebidoDemasiado;
     }
 }
